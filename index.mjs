cca1af91179fa57e512fd1d27fa751850bd39071
Authapon R. Kongwan <authapon@gmail.com>
2024-07-16 12:23:54 +0700

    add prettier for dev dependency

--- index.mjs
+++ index.mjs
@@ -1,54 +1,67 @@
-import {createHmac} from 'node:crypto';
+import { createHmac } from "node:crypto";
 
-const getTimex = (timenow)=>{
-	var [date, month, year] =[timenow.getDate(), timenow.getMonth()+1, timenow.getFullYear()];
-	var [hour, minute, second] = [timenow.getHours(), timenow.getMinutes(), timenow.getSeconds()];
-	if(month < 10){
-		month = "0"+month;
-	} 
-	if(date < 10){
-		date = "0"+date;
-	}
-	if(hour < 10) {
-		hour = "0"+hour;
-	}
-	if(minute < 10){
-		minute = "0"+minute;
-	}
-	if(second < 10){
-		second = "0"+second;
-	}
-	return year+"-"+month+"-"+date+"T"+hour+":"+minute+":"+second;
-}
+const getTimex = (timenow) => {
+  var [date, month, year] = [
+    timenow.getDate(),
+    timenow.getMonth() + 1,
+    timenow.getFullYear(),
+  ];
+  var [hour, minute, second] = [
+    timenow.getHours(),
+    timenow.getMinutes(),
+    timenow.getSeconds(),
+  ];
+  if (month < 10) {
+    month = "0" + month;
+  }
+  if (date < 10) {
+    date = "0" + date;
+  }
+  if (hour < 10) {
+    hour = "0" + hour;
+  }
+  if (minute < 10) {
+    minute = "0" + minute;
+  }
+  if (second < 10) {
+    second = "0" + second;
+  }
+  return (
+    year + "-" + month + "-" + date + "T" + hour + ":" + minute + ":" + second
+  );
+};
 
-const OTP = (secret, size)=>{
-	return OTPx(secret, size, new Date());
-}
+const OTP = (secret, size) => {
+  return OTPx(secret, size, new Date());
+};
 
-const OTPx = (secret, size, timenow)=>{
-	const hmac = createHmac('sha512', getTimex(timenow));
-	return hmac.update(secret).digest('hex').slice(0, size*2);
-}
+const OTPx = (secret, size, timenow) => {
+  const hmac = createHmac("sha512", getTimex(timenow));
+  return hmac
+    .update(secret)
+    .digest("hex")
+    .slice(0, size * 2);
+};
 
-const CheckOTP = (otp, secret, size)=>{
-	return CheckOTPx(otp, secret, size, 5);
-}
+const CheckOTP = (otp, secret, size) => {
+  return CheckOTPx(otp, secret, size, 5);
+};
 
-const CheckOTPx = (otp, secret, size, time1)=>{
-	const timenow = new Date();
-	for(let i = 0; i <= time1; i++){
-		let otpx = OTPx(secret, size, new Date(timenow.getTime()-(i*1000)));
-		if(otpx==otp) {
-			return true;
-		}
-	}
-	for(let i = 1; i <= time1; i++){
-		let otpx = OTPx(secret, size, new Date(timenow.getTime()+(i*1000)));
-		if(otpx==otp) {
-			return true;
-		}
-	}
-	return false;
-}
+const CheckOTPx = (otp, secret, size, time1) => {
+  const timenow = new Date();
+  for (let i = 0; i <= time1; i++) {
+    let otpx = OTPx(secret, size, new Date(timenow.getTime() - i * 1000));
+    if (otpx == otp) {
+      return true;
+    }
+  }
+  for (let i = 1; i <= time1; i++) {
+    let otpx = OTPx(secret, size, new Date(timenow.getTime() + i * 1000));
+    if (otpx == otp) {
+      return true;
+    }
+  }
+  return false;
+};
 
-export { getTimex, OTP, OTPx, CheckOTP, CheckOTPx }
+export { getTimex, OTP, OTPx, CheckOTP, CheckOTPx };
