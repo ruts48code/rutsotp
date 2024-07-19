9cad751c09da42f53c21df28814206f586a83aa6
Authapon R. Kongwan <authapon@gmail.com>
2024-07-19 10:54:10 +0700

    update to crypto web

--- index.mjs
+++ index.mjs
@@ -1,67 +1,116 @@
-import { createHmac } from "node:crypto";
+const getTimex = async (timenow) => {
+  return new Promise(async (resolve) => {
+    var [date, month, year] = [
+      timenow.getDate(),
+      timenow.getMonth() + 1,
+      timenow.getFullYear(),
+    ];
+    var [hour, minute, second] = [
+      timenow.getHours(),
+      timenow.getMinutes(),
+      timenow.getSeconds(),
+    ];
+    if (month < 10) {
+      month = "0" + month;
+    }
+    if (date < 10) {
+      date = "0" + date;
+    }
+    if (hour < 10) {
+      hour = "0" + hour;
+    }
+    if (minute < 10) {
+      minute = "0" + minute;
+    }
+    if (second < 10) {
+      second = "0" + second;
+    }
+    resolve(
+      year +
+        "-" +
+        month +
+        "-" +
+        date +
+        "T" +
+        hour +
+        ":" +
+        minute +
+        ":" +
+        second,
+    );
+  });
+};
 
-const getTimex = (timenow) => {
-  var [date, month, year] = [
-    timenow.getDate(),
-    timenow.getMonth() + 1,
-    timenow.getFullYear(),
-  ];
-  var [hour, minute, second] = [
-    timenow.getHours(),
-    timenow.getMinutes(),
-    timenow.getSeconds(),
-  ];
-  if (month < 10) {
-    month = "0" + month;
-  }
-  if (date < 10) {
-    date = "0" + date;
-  }
-  if (hour < 10) {
-    hour = "0" + hour;
-  }
-  if (minute < 10) {
-    minute = "0" + minute;
-  }
-  if (second < 10) {
-    second = "0" + second;
-  }
-  return (
-    year + "-" + month + "-" + date + "T" + hour + ":" + minute + ":" + second
+const Delay = async (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
+const BufferToHex = async (buffer) => {
+  return Array.from(new Uint8Array(buffer))
+    .map((b) => b.toString(16).padStart(2, "0"))
+    .join("");
+};
+
+const hmacKey = async (key) => {
+  const enc = new TextEncoder();
+  const keyEnc = enc.encode(key);
+
+  return await crypto.subtle.importKey(
+    "raw",
+    keyEnc,
+    {
+      name: "HMAC",
+      hash: "SHA-256",
+    },
+    true,
+    ["sign", "verify"],
   );
 };
 
-const OTP = (secret, size) => {
-  return OTPx(secret, size, new Date());
+const OTP = async (secret) => {
+  return await OTPx(secret, new Date());
 };
 
-const OTPx = (secret, size, timenow) => {
-  const hmac = createHmac("sha512", getTimex(timenow));
-  return hmac
-    .update(secret)
-    .digest("hex")
-    .slice(0, size * 2);
+const OTPx = async (secret, timenow) => {
+  return new Promise(async (resolve) => {
+    const secretKey = await hmacKey(secret);
+    const dataEnc = new TextEncoder().encode(await getTimex(timenow));
+    let output = await crypto.subtle.sign("HMAC", secretKey, dataEnc);
+    let outputHex = BufferToHex(output);
+    resolve(outputHex);
+  });
 };
 
-const CheckOTP = (otp, secret, size) => {
-  return CheckOTPx(otp, secret, size, 5);
+const CheckOTP = async (otp, secret) => {
+  return await CheckOTPx(otp, secret, 5);
 };
 
-const CheckOTPx = (otp, secret, size, time1) => {
-  const timenow = new Date();
-  for (let i = 0; i <= time1; i++) {
-    let otpx = OTPx(secret, size, new Date(timenow.getTime() - i * 1000));
-    if (otpx == otp) {
-      return true;
+const CheckOTPx = async (otp, secret, time1) => {
+  return new Promise(async (resolve) => {
+    const timenow = new Date();
+    for (let i = 0; i <= time1; i++) {
+      let otpx = await OTPx(secret, new Date(timenow.getTime() - i * 1000));
+      if (otpx == otp) {
+        resolve(true);
+      }
     }
-  }
-  for (let i = 1; i <= time1; i++) {
-    let otpx = OTPx(secret, size, new Date(timenow.getTime() + i * 1000));
-    if (otpx == otp) {
-      return true;
+    for (let i = 1; i <= time1; i++) {
+      let otpx = await OTPx(secret, new Date(timenow.getTime() + i * 1000));
+      if (otpx == otp) {
+        resolve(true);
+      }
     }
-  }
-  return false;
+    resolve(false);
+  });
 };
 
-export { getTimex, OTP, OTPx, CheckOTP, CheckOTPx };
+export {
+  getTimex,
+  hmacKey,
+  BufferToHex,
+  Delay,
+  OTP,
+  OTPx,
+  CheckOTP,
+  CheckOTPx,
+};
