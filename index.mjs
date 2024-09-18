2be95e1fe4283d1a5fbd5d7f3e26583a85969b26
Authapon Kongwan <authapon@gmail.com>
2024-09-18 19:09:07 +0700

    change to use unixtime

--- index.mjs
+++ index.mjs
@@ -1,42 +1,6 @@
 export const getTimex = async (timenow) => {
   return new Promise(async (resolve) => {
-    var [date, month, year, hour, minute, second] = [
-      timenow.getDate(),
-      timenow.getMonth() + 1,
-      timenow.getFullYear(),
-      timenow.getHours(),
-      timenow.getMinutes(),
-      timenow.getSeconds(),
-    ];
-
-    if (month < 10) {
-      month = "0" + month;
-    }
-    if (date < 10) {
-      date = "0" + date;
-    }
-    if (hour < 10) {
-      hour = "0" + hour;
-    }
-    if (minute < 10) {
-      minute = "0" + minute;
-    }
-    if (second < 10) {
-      second = "0" + second;
-    }
-    resolve(
-      year +
-        "-" +
-        month +
-        "-" +
-        date +
-        "T" +
-        hour +
-        ":" +
-        minute +
-        ":" +
-        second,
-    );
+    resolve(Math.floor(timenow/1000).toString())
   });
 };
 
